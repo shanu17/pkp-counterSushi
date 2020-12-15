5142846931d4beb8a0322ad5999cabde984a98d9
shanu17 <SUK117@INFOST10.univ.pitt.edu>
2020-12-15 14:31:21 -0500

    Adding errors and parsing according to COUNTER SUSHI documentation

--- CounterSushiPlugin.inc.php
+++ CounterSushiPlugin.inc.php
@@ -14,6 +14,11 @@
 
 import('lib.pkp.classes.plugins.GenericPlugin');
 
+define('COUNTER_SUSHI_ERROR_SEVERITY_INFO', 1);
+define('COUNTER_SUSHI_ERROR_SEVERITY_WARNING', 2);
+define('COUNTER_SUSHI_ERROR_SEVERITY_ERROR', 4);
+define('COUNTER_SUSHI_ERROR_SEVERITY_FATAL', 8);
+
 class CounterSushiPlugin extends GenericPlugin{
 	
 	/**
@@ -21,7 +26,8 @@ class CounterSushiPlugin extends GenericPlugin{
 	 */
 	public function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path);
-		if ($success && $this->getEnabled()) {
+		$x = $this->getEnabled();
+		if ($success && $x) {
 			HookRegistry::register('APIHandler::endpoints', [$this, 'endpoints']);
 		}
 		return $success;
@@ -31,16 +37,16 @@ class CounterSushiPlugin extends GenericPlugin{
 	 * @copydoc PKPPlugin::getDisplayName
 	 */
 	public function getDisplayName() {
-		return __('plugins.generic.sushi.name');
+		return __('plugins.generic.counter.sushi.name');
 	}
 
 	/**
 	 * @copydoc PKPPlugin::getDescription
 	 */
 	public function getDescription() {
-		return __('plugins.generic.sushi.description');
+		return __('plugins.generic.counter.sushi.description');
 	}
-	
+
 	/**
 	 * Add endpoints to the stats APIHandler to serve the
 	 * COUNTER reports
@@ -106,21 +112,29 @@ class CounterSushiPlugin extends GenericPlugin{
 		if (!$request->getContext()) {
 			return $response->withStatus(404)->withJsonError('api.404.resourceNotFound');
 		}
+		if(!isset($slimRequest->getQueryParams()['customer_id'])) {
+			$error = $this->createError(1030, COUNTER_SUSHI_ERROR_SEVERITY_FATAL, 'Insufficient Information to Process Request');
+			return $response->withStatus(400)->withJsonError($error);
+		}
+		if($slimRequest->getQueryParams()['customer_id'] !== 'anonymous') {
+			$error = $this->createError(2010, COUNTER_SUSHI_ERROR_SEVERITY_ERROR, 'Requestor is Not Authorized to Access Usage for Institution');
+			return $response->withStatus(403)->withJsonError($error);
+		}
 		$report = end(explode('/', $url));
 		switch ($report) {
 			case 'reports':
-				return $response->withJson([
-					'Report_Header' => [
-						'Created' => date('c', time()),
-						'Created_By' => '...',
-						'Release' => 5,
-					],
-					'Report_Items' => $this->getReports()
-				]);
+				$availableReports = $this->getReports();
+				$formattedReports = array();
+				foreach($availableReports as $ar)
+					$formattedReports[] = array('Report_Name' => '...', 'Report_ID' => strtoupper($ar), 'Release' => 5, 'Report_Description' => '...', 'Path' => '/reports/' . $ar);
+				return $response->withJson(
+					$formattedReports					
+				);
 			case 'status':
-				return $response->withStatus(400)->withJsonError('plugins.generic.sushi.api.statusNotImplemented');
-			case 'members':
-				return $response->withStatus(400)->withJsonError('plugins.generic.sushi.api.membersNotImplemented');
+				return $response->withJson([
+					'Description' => '...',
+					'Service_Active' => TRUE,
+				]);
 			default:
 				$availableReports = $this->getReports();
 				if (in_array($report, $availableReports)) {
@@ -136,9 +150,16 @@ class CounterSushiPlugin extends GenericPlugin{
 					} else {
 						$class = 'CounterReport' . strtoupper($report);
 						import('plugins.generic.counterSushi.reports.' . $class);
+						$reportFilters = array();
+						foreach($args as $x => $xValue) {
+							if($x == 'assocTypes')
+								continue;
+							$filter = array('Name' => $x, 'Value' => $xValue);
+							$reportFilters[] = $filter; 
+						}
 						$counterReport = new $class();
 						$rows = $counterReport->getReport($args);
-						return $response->withJson([
+						return $response->withHeader('Content-type', 'application/json')->withJson([
 							'Report_Header' => [
 								'Created' => date('c', time()),
 								'Created_By' => '...',
@@ -147,24 +168,7 @@ class CounterSushiPlugin extends GenericPlugin{
 								'Release' => 5,
 								'Report_Name' => '...',
 								'Institution_Name' => '...',
-								'Report_Filters' => [
-									[
-										'Name' => 'Platform',
-										'Value' => '...',
-									],
-									[
-										'Name' => 'Begin_Date',
-										'Value' => $args['begin_date'],
-									],
-									[
-										'Name' => 'End_Date',
-										'Value' => $args['end_date'],
-									],
-									[
-										'Name' => 'Data_Type',
-										'Value' => 'Journal',
-									],
-								],
+								'Report_Filters' => $reportFilters,
 							],
 							'Report_Items' => $rows
 						]);
@@ -192,52 +196,74 @@ class CounterSushiPlugin extends GenericPlugin{
 	/**
 	 * Get names of all the reports available
 	 * 
-	 * @return string array
+	 * @return string array of all available report or NULL
 	 */
 	public function getReports() {
 		$reportsPath = $this->getPluginPath() . DIRECTORY_SEPARATOR . 'reports';
 		$reportFiles = preg_grep('/^CounterReport/', scandir($reportsPath));
-		foreach ($reportFiles as &$rf) {
-			$rf = str_split(explode('.', strtolower($rf))[0], 13)[1];
+		if($reportFiles) {
+			foreach ($reportFiles as &$rf) {
+				$rf = str_split(explode('.', strtolower($rf))[0], 13)[1]; 
+			}
+			unset($rf);
+			return $reportFiles;
 		}
-		unset($rf);
-		return $reportFiles;
+		return null;
 	}
 	
 	/**
 	 * Parse the Query string to allow for proper report fetching
 	 * 
 	 * @param array $args A mix of default parameters and query string parameters
-	 * @return mixed Array of parsed parameters ready to be sent to the Counter report
+	 * @return mixed Array of parsed parameters ready to be sent to the Counter report or error
 	 */
 	public function parseParams($requestParams) {
-		if (empty($requestParams['customer_id']) || $requestParams['customer_id'] !== 'anonymous') {
-			$args = [
-				'error' => 'plugins.generic.sushi.api.invalidCustomerId',
-			];
-		} else if (empty($requestParams['begin_date']) || is_null($beginDateFormat = $this->validFormatDate($requestParams['begin_date']))
-				|| empty($requestParams['end_date']) || is_null($endDateFormat = $this->validFormatDate($requestParams['end_date']))) {
-			$args = [
-				'error' => 'plugins.generic.sushi.api.invalidDates',
-			];
-		} else if (!is_int($requestParams['count']) || $requestParams['count'] > 100 || $requestParams['count'] < 1) {
-			$args = [
-				'error' => 'plugins.generic.sushi.api.invalidCount',
-			];
-		} else if (!is_int($requestParams['position_token']) || $requestParams['position_token'] < 0) {
-			$args = [
-				'error' => 'plugins.generic.sushi.api.invalidPositionToken',
-			];
-		} else {
-			$args = [
-				'count' => $requestParams['count'],
-				'dateStart' => DateTime::createFromFormat($beginDateFormat, $requestParams['begin_date'])->format($beginDateFormat),
-				'dateEnd' => DateTime::createFromFormat($beginDateFormat, $requestParams['end_date'])->format($endDateFormat),
-				'offset' => $requestParams['position_token'],
-				'assocTypes' => [ASSOC_TYPE_SUBMISSION, ASSOC_TYPE_SUBMISSION_FILE],
+		if(empty($requestParams['begin_date']) || empty($requestParams['end_date'])) {
+			return $args = [
+				'error' => $this->createError(3070, COUNTER_SUSHI_ERROR_SEVERITY_ERROR, 'Required ReportFilter Missing'),
 			];
 		}
-		return $args;
+		if (is_null($beginDateFormat = $this->validFormatDate($requestParams['begin_date'])) || is_null($endDateFormat = $this->validFormatDate($requestParams['end_date']))) {
+			return $args = [
+				'error' => $this->createError(3020, COUNTER_SUSHI_ERROR_SEVERITY_ERROR, 'Invalid Date Arguments'),
+			];
+		}
+		if (!is_int($requestParams['count']) || $requestParams['count'] > 100 || $requestParams['count'] < 1) {
+			return $args = [
+				'error' => $this->createError(3060, COUNTER_SUSHI_ERROR_SEVERITY_ERROR, 'Invalid ReportFilter Value', null, 'plugins.generic.sushi.api.invalidCount'),
+			];
+		}
+		if (!is_int($requestParams['position_token']) || $requestParams['position_token'] < 0) {
+			return $args = [
+				'error' => $this->createError(3060, COUNTER_SUSHI_ERROR_SEVERITY_ERROR, 'Invalid ReportFilter Value', null, 'plugins.generic.sushi.api.invalidPositionToken'),
+			];
+		}
+		return $args = [
+			'count' => $requestParams['count'],
+			'dateStart' => DateTime::createFromFormat($beginDateFormat, $requestParams['begin_date'])->format($beginDateFormat),
+			'dateEnd' => DateTime::createFromFormat($beginDateFormat, $requestParams['end_date'])->format($endDateFormat),
+			'offset' => $requestParams['position_token'],
+			'assocTypes' => [ASSOC_TYPE_SUBMISSION, ASSOC_TYPE_SUBMISSION_FILE],
+			'contextIds' => [],
+		];
+	}
+	
+	public function createError($number, $severity, $message = '', $helpUrl = NULL, $data = NULL) {
+		$severityString = '';
+		switch ($severity) {
+			case COUNTER_SUSHI_ERROR_SEVERITY_INFO:
+				$severityString = 'Info/Debug';
+				break;
+			case COUNTER_SUSHI_ERROR_SEVERITY_WARNING:
+				$severityString = 'Warning';
+				break;
+			case COUNTER_SUSHI_ERROR_SEVERITY_ERROR:
+				$severityString = 'Error';
+				break;
+			case COUNTER_SUSHI_ERROR_SEVERITY_FATAL:
+				$severityString = 'Fatal';
+		}
+		$error = array('Code' => $number, 'Severity' => $severityString, 'Message' => $message, 'Help_URL' => $helpUrl, 'Data' => $data);
+		return $error;
 	}
 }
-
